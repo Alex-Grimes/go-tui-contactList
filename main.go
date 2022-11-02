9a4ccf2e3c54ee0157c0f2dde0ad6529a3e195eb
Alex <AlexGrimes.se@gmail.com>
2022-11-02 15:29:31 -0400

    added in the list of states for dropdown

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"github.com/rivo/tview"
 )
 
+var states = []string{"AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
+	"HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME",
+	"MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM",
+	"NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX",
+	"UT", "VA", "VT", "WA", "WI", "WV", "WY"}
+
 type Contact struct {
 	firstName   string
 	lastName    string
